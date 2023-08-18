9ec26912c1d564f8351b54db40529c5142971a96
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-08-18 20:41:59 +0300

    Initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by jazeps.ivulis on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
