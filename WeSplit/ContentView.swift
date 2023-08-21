6a90b496aa9627ee59029024fb5a51f2c71ff662
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-08-21 14:29:55 +0300

    Created examples of Day 16 lessons

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var name = ""
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
+
+                Section {
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                }
+                
+                Button("Tap Count: \(tapCount)") {
+                    tapCount += 1
+                }
+                
+                TextField("Enter your name", text: $name)
+                Text("Your name is \(name)")
+                
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+            }
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
